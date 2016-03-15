6f4801276c5ec7d0014fc0b42436b5a9f499d9c3
chenshuiluke <chenshuiluke@gmail.com>
2016-03-15 00:42:36 -0500

    Made MD5 a default filter. Converted from listview to tree view. Now scans directories for duplicate files, instead of scanning directories for a predefined file. Duplicates are listed in a tree

--- src/main/java/DSProjectController.java
+++ src/main/java/DSProjectController.java
@@ -14,13 +14,19 @@ import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.collections.FXCollections;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.TreeItem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
 
 public class DSProjectController {
 
-	private File originalFile = null;
+	private ArrayList<File> fileList = new ArrayList<>();
 	private File searchDirectory = null;
-	private String originalMD5 = "";
-	private boolean filterMD5 = false;
+	private boolean filterMD5 = true;
 	private boolean filterSize = false;
 
 
@@ -43,28 +49,19 @@ public class DSProjectController {
     void toggleMD5Filter() {
 		filterMD5 = !filterMD5;
 		clearList();
-		populateList(searchDirectory);
+		populateTreeViewAndRemoveExcess(searchDirectory);
     }
 
     @FXML
     void toggleSizeFilter() {
 		filterSize = !filterSize;
 		clearList();
-		populateList(searchDirectory);
+		populateTreeViewAndRemoveExcess(searchDirectory);
     }
 
 
     @FXML
     private Button folderButton;
-    @FXML
-    private Text originalFileName;
-
-    @FXML
-    private Text originalFileSize;
-
-    @FXML
-    private Text originalFileHash;
-
     @FXML
     private Text statusText;
 
@@ -75,16 +72,27 @@ public class DSProjectController {
     private URL location;
 
     @FXML
-    private ListView<String> duplicateList;
+    private TreeView<String> duplicateList;
 
     @FXML
     void getSelected(){
-		String item = duplicateList.getSelectionModel().getSelectedItem();
-		if(item != null){
-			File file = new File(item);
-			selectedFileName.setText("Name: " + file.getName());
-			selectedFileSize.setText("Size: " + file.length() + " bytes");
-			selectedFileHash.setText("Hash: " + getMD5(file));
+		TreeItem<String> treeItem= duplicateList.getSelectionModel().getSelectedItem();
+		if(treeItem != null){
+			String item = treeItem.getValue();
+			
+			if(item != null){
+				File file = new File(item);
+				if(file.isFile()){
+					selectedFileName.setText("Name: " + file.getName());
+					selectedFileSize.setText("Size: " + file.length() + " bytes");
+					selectedFileHash.setText("Hash: " + getMD5(file));
+				}
+				else{
+					selectedFileName.setText("Name: ");
+					selectedFileSize.setText("Size: ");
+					selectedFileHash.setText("Hash: ");
+				}
+			}
 		}
     }
 	private String getMD5(File file){
@@ -99,24 +107,55 @@ public class DSProjectController {
 		}
 		return md5;
 	}
-    @FXML
-    void selectFile(ActionEvent event) {
-		FileChooser chooser = new FileChooser();
-		chooser.setTitle("Choose the original file");
-		File file = chooser.showOpenDialog(null);
-		if(file != null){
-			originalFile = file;	
-			originalFileName.setText("Name: " + originalFile.getName());
-			originalFileSize.setText("Size: " + String.valueOf(originalFile.length() + " bytes"));
-			originalFileHash.setText("Hash: " + getMD5(originalFile));
-			originalMD5 = getMD5(originalFile);
-			folderButton.setDisable(false);
-		}
-    }
 	private void clearList(){
-		duplicateList.setItems(FXCollections.observableArrayList()); //Empties the current duplicate list
+		duplicateList.getRoot().getChildren().setAll(FXCollections.observableArrayList()); //Empties the current duplicate list
 	}
-
+	ArrayList<File> getFileList(File file){
+		ArrayList list = new ArrayList<>();
+		if(file.isFile()){
+			list.add(file);
+		}
+		else if(file.isDirectory()){
+			String[] files = file.list();
+			if(files != null){
+				for(String entry : files){
+					System.out.println(entry);
+					File temp = new File(file, entry);
+					list.addAll(getFileList(temp));
+				}
+			}
+		}
+		return list;
+	}
+	void populateTreeViewAndRemoveExcess(File file){
+		fileList = getFileList(searchDirectory);
+		for(File singleFile : fileList){
+			System.out.println(singleFile);
+			TreeItem<String> fileItem = new TreeItem<String> (singleFile.getName());
+			fileItem.getChildren().add(new TreeItem<String>(singleFile.getAbsoluteFile().toString()));
+			ArrayList<String> duplicates = populateList(singleFile, searchDirectory);
+			for(String duplicate : duplicates){
+				TreeItem<String> item = new TreeItem<String>(duplicate);
+				fileItem.getChildren().add(item);
+			}
+			duplicateList.getRoot().getChildren().add(fileItem);
+		}
+		ArrayList<TreeItem> excess = new ArrayList<>();
+		ObservableList<TreeItem<String>> nodeList= duplicateList.getRoot().getChildren();
+		for(int counter1 = 0; counter1 < nodeList.size(); counter1++){
+			if(nodeList.get(counter1).getChildren().size() < 1){
+					excess.add(nodeList.get(counter1));
+					continue;
+			}
+			for(int counter2 = counter1+1; counter2 < nodeList.size(); counter2++){
+				if(nodeList.get(counter2).getValue().equals(nodeList.get(counter1).getValue())){
+					System.out.println("equals");
+					excess.add(nodeList.get(counter2));
+				}
+			}
+		}
+		nodeList.removeAll(excess);
+	}
     @FXML
     void selectFolder(ActionEvent event) {
 		DirectoryChooser chooser = new DirectoryChooser();
@@ -125,38 +164,39 @@ public class DSProjectController {
 		if(directory != null){
 			searchDirectory = directory; 
 			clearList();
-			populateList(searchDirectory);
+			populateTreeViewAndRemoveExcess(searchDirectory);
 			sizeCheckBox.setDisable(false);
 			md5CheckBox.setDisable(false);
 		}
 	}
-	boolean verifyMD5(File file){
-		return getMD5(file).equals(originalMD5);
+	boolean verifyMD5(File origin, File file){
+		return getMD5(file).equals(getMD5(origin));
 	}
-	boolean isAbsoluteFilePathEqual(File file){
-		return file.getAbsoluteFile().toString().equals(originalFile.getAbsoluteFile().toString());
+	boolean isAbsoluteFilePathEqual(File origin, File file){
+		return file.getAbsoluteFile().toString().equals(origin.getAbsoluteFile().toString());
 	}
-	void populateList(File file){
+	ArrayList<String> populateList(File origin, File file){
 		//add file only
+		ArrayList<String> list = new ArrayList<>();
 		if(file.isFile()){
-			if(!isAbsoluteFilePathEqual(file)){
+			if(!isAbsoluteFilePathEqual(origin, file)){
 				if(filterSize && filterMD5){
-					if(file.length() == originalFile.length() && verifyMD5(file)){
-							duplicateList.getItems().add(file.getAbsoluteFile().toString());	
+					if(file.length() == origin.length() && verifyMD5(origin, file)){
+						list.add(file.getAbsoluteFile().toString());
 					}
 				}
 				else if(filterSize){
-					if(file.length() == originalFile.length()){
-						duplicateList.getItems().add(file.getAbsoluteFile().toString());	
+					if(file.length() == origin.length()){
+						list.add(file.getAbsoluteFile().toString());
 					}
 				}
 				else if(filterMD5){
-					if(verifyMD5(file)){
-						duplicateList.getItems().add(file.getAbsoluteFile().toString());	
+					if(verifyMD5(origin, file)){
+						list.add(file.getAbsoluteFile().toString());
 					}
 				}
 				else{
-					duplicateList.getItems().add(file.getAbsoluteFile().toString());	
+						list.add(file.getAbsoluteFile().toString());
 				}
 			}
 			
@@ -166,14 +206,20 @@ public class DSProjectController {
 			if(subNote != null){
 				for(String filename : subNote){
 					File temp = new File(file, filename);
-					populateList(temp);
+					ArrayList subFiles = populateList(origin, temp);
+					list.addAll(subFiles);
 				}
 			}
 		}
+		return list;
 	}
     @FXML
     void initialize() {
         assert duplicateList != null : "fx:id=\"duplicateList\" was not injected: check your FXML file 'DSProject.fxml'.";
+		TreeItem<String> rootItem = new TreeItem<String> ("Duplicates");
+		rootItem.setExpanded(true);
+		duplicateList.setRoot(rootItem);
+		
 
     }
 }
